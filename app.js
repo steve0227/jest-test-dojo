943b4d303adbe5745c650676baa56b4bd21a558d
Steve <steve.blandon@udea.edu.co>
2020-05-23 11:29:44 -0500

    git ignore cverage

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app= express();
 const cal = require('./calculator');
 const CodeBreaker = require('./codebreaker');
 
+
 app.get('/add',(req,res)=>{
     res.json({
         result: cal.add(parseInt(req.query.value1),parseInt(req.query.value2))
@@ -17,9 +18,12 @@ app.get('/subtract',(req,res)=>{
 
 app.get('/codebreaker',(req,res)=>{
     CodeBreaker.setSecret();
+    var cod =CodeBreaker.guess(req.query.guess)
+    /*if(cod==='XXXX')
+    CodeBreaker.randomSecret();*/
     res.json({
         guessing: req.query.guess,
-        result: CodeBreaker.guess(req.query.guess)
+        result: cod
     });
 });
 
